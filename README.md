b049360522b0b08304134d3855c7e876919c8de4
Shawn <sppang1@yahoo.com>
2019-07-01 12:52:20 -0400

    Changed wording of index, about, and skills, cleaned up code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# PangPortfolio
+# Pang Portfolio
 This is a portfolio webpage I created to demonstrate my ablities with HTML and CSS
--- loadHeader.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $('#nav').load('navbar.html');
-})
-
-$("#aboutLink").append( "<span class='sr-only'>(current)</span>" );
-$("#aboutList").addClass("nav-item active");
